3f1a3373fd6d75926a4ced20a2750ac61153c87c
efeytas <efeytas@gmail.com>
2025-07-29 20:16:45 +0300

    localization, images and scrpit added

---
+++ lib/core/locale_keys.g.dart
@@ -0,0 +1,14 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: constant_identifier_names
+
+abstract class  LocaleKeys {
+  static const onboarding_firstPageTitle = 'onboarding.firstPageTitle';
+  static const onboarding_firstPageSubTitle = 'onboarding.firstPageSubTitle';
+  static const onboarding_legal_text = 'onboarding.legal_text';
+  static const onboarding = 'onboarding';
+  static const buttons_getStarted = 'buttons.getStarted';
+  static const buttons_continue = 'buttons.continue';
+  static const buttons = 'buttons';
+
+}
--- lib/shared/utils/image_constant.dart
+++ lib/shared/utils/image_constant.dart
@@ -1,5 +1,11 @@
 class ImageConstant {
 
   static String imagePath = 'assets/images';
+
+  static String imgOnboarding1 = '$imagePath/onboarding1.png';
+
+  static String imgOnboarding2 = '$imagePath/onboarding2.png';
+
+  static String imgOnboarding3 = '$imagePath/onboarding3.png';
   
 }
