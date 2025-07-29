67fe074100ef81944b046e9b5181e0e96142069d
efeytas <efeytas@gmail.com>
2025-07-29 18:52:03 +0300

    get_it_dependencies added

---
+++ lib/features/home/services/api_home_service.dart
@@ -0,0 +1,6 @@
+import 'package:injectable/injectable.dart';
+import 'package:plantapp/features/home/services/home_service.dart';
+
+@Singleton(as: HomeService)
+class ApiHomeService implements HomeService {
+}
