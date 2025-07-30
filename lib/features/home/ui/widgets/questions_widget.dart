cf4bb4140e2d508d430fa40e5ae9741fb9cf95d3
efeytas <efeytas@gmail.com>
2025-07-31 00:12:04 +0300

    text added on question

--- lib/features/home/ui/widgets/questions_widget.dart
+++ lib/features/home/ui/widgets/questions_widget.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:plantapp/features/home/models/response/questions_response.dart';
+import 'package:plantapp/shared/theme/custom_text_style.dart';
 import 'package:plantapp/shared/utils/size_utils.dart';
 import 'package:plantapp/shared/widgets/custom_image_view.dart';
 
@@ -13,12 +14,28 @@ class QuestionsWidget extends StatelessWidget {
       width: 240.h,
       child: Stack(
         children: [
-          
           ClipRRect(
             borderRadius: BorderRadius.circular(12),
             child: CustomImageView(
               imagePath: questionsResponse?.imagePath ?? "",
               fit: BoxFit.cover,
+            ),
+          ),
+
+          Positioned(
+            bottom: (questionsResponse?.title?.length ?? 0) >= 25 ? 15.v : 25.v,
+            left: 14.h,
+            right: 14.h,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              width: 240.h,
+              child: Text(
+                questionsResponse?.title ?? "",
+                style: CustomTextStyle.titleMedium?.copyWith(color: Colors.white,
+                fontSize: 15.fSize,),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
         ],
